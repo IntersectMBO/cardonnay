fe6a914977f9b2f625be63cafa25dc6941ab4112
Martin Kourim <martin.kourim@iohk.io>
2025-06-03 15:35:49 +0200

    refactor: Major refactor and improvements

---
+++ src/cardonnay/cli_control.py
@@ -0,0 +1,141 @@
+import json
+import logging
+import pathlib as pl
+
+from cardonnay import cli_utils
+from cardonnay import helpers
+
+LOGGER = logging.getLogger(__name__)
+
+STATUS_STARTED = "status_started"
+
+
+def testnet_stop(statedir: pl.Path, env: dict) -> int:
+    if not cli_utils.check_env_sanity():
+        return 1
+
+    stop_script = statedir / "stop-cluster"
+    if not stop_script.exists():
+        LOGGER.error(f"Stop script '{stop_script}' does not exist.")
+        return 1
+
+    cli_utils.set_env_vars(env=env)
+
+    LOGGER.info(f"Stopping testnet with `{stop_script}`.")
+    try:
+        helpers.run_command(str(stop_script), workdir=statedir)
+    except RuntimeError:
+        LOGGER.exception("Failed to stop testnet")
+        return 1
+
+    return 0
+
+
+def testnet_restart_nodes(statedir: pl.Path, env: dict) -> int:
+    if not cli_utils.check_env_sanity():
+        return 1
+
+    script = statedir / "supervisorctl_restart_nodes"
+    if not script.exists():
+        LOGGER.error(f"Restart nodes script '{script}' does not exist.")
+        return 1
+
+    cli_utils.set_env_vars(env=env)
+
+    LOGGER.info(f"Restarting testnet nodes with `{script}`.")
+    try:
+        helpers.run_command(str(script), workdir=statedir)
+    except RuntimeError:
+        LOGGER.exception("Failed to restart testnet nodes")
+        return 1
+
+    return 0
+
+
+def testnet_restart_all(statedir: pl.Path, env: dict) -> int:
+    if not cli_utils.check_env_sanity():
+        return 1
+
+    script = statedir / "supervisorctl"
+    if not script.exists():
+        LOGGER.error(f"The supervisorctl script '{script}' does not exist.")
+        return 1
+
+    cli_utils.set_env_vars(env=env)
+
+    cmd = f"{script} restart all"
+    LOGGER.info(f"Restarting testnet with `{cmd}`.")
+    try:
+        helpers.run_command(cmd, workdir=statedir)
+    except RuntimeError:
+        LOGGER.exception("Failed to restart testnet")
+        return 1
+
+    return 0
+
+
+def print_instances(workdir: pl.Path) -> None:
+    running_instances = sorted(cli_utils.get_running_instances(workdir=workdir))
+
+    print("Running instances:")
+    out_list = []
+    for i in running_instances:
+        statedir = workdir / f"state-cluster{i}"
+        try:
+            with open(statedir / "testnet.json", encoding="utf-8") as fp_in:
+                testnet_info = json.load(fp_in)
+        except Exception:
+            testnet_info = {}
+        testnet_name = testnet_info.get("name") or "unknown"
+        testnet_status = "started" if (statedir / STATUS_STARTED).exists() else "starting"
+        out_list.append({"instance": i, "type": testnet_name, "status": testnet_status})
+    print(json.dumps(out_list, indent=2))
+
+
+def print_env_sh(env: dict[str, str]) -> None:
+    content = [f'export {var_name}="{val}"' for var_name, val in env.items()]
+    print("\n".join(content))
+
+
+def cmd_control(
+    work_dir: str,
+    instance_num: int,
+    stop: bool = False,
+    restart: bool = False,
+    restart_nodes: bool = False,
+    inspect: bool = False,
+    print_env: bool = False,
+) -> int:
+    workdir = cli_utils.get_workdir(workdir=work_dir)
+    workdir_abs = workdir.absolute()
+
+    if instance_num < 0:
+        LOGGER.error("Valid instance number is required.")
+        return 1
+
+    statedir = workdir_abs / f"state-cluster{instance_num}"
+    env = cli_utils.create_env_vars(workdir=workdir_abs, instance_num=instance_num)
+
+    if stop:
+        testnet_stop(statedir=statedir, env=env)
+    elif restart:
+        testnet_restart_all(statedir=statedir, env=env)
+    elif restart_nodes:
+        testnet_restart_nodes(statedir=statedir, env=env)
+    elif print_env:
+        print_env_sh(env=env)
+    elif inspect:
+        pass
+    else:
+        LOGGER.error("No valid action was selected.")
+        return 1
+
+    return 0
+
+
+def cmd_ls(work_dir: str) -> int:
+    workdir = cli_utils.get_workdir(workdir=work_dir)
+    workdir_abs = workdir.absolute()
+
+    print_instances(workdir=workdir_abs)
+    return 0
--- src/cardonnay/main.py
+++ src/cardonnay/main.py
@@ -5,24 +5,51 @@ import typing as tp
 
 import click
 
-from cardonnay import cli
+from cardonnay import cli_control
+from cardonnay import cli_generate
+from cardonnay import cli_utils
 
 LOGGER = logging.getLogger(__name__)
 
 
 def common_options(func: tp.Callable) -> tp.Callable:
     """Add shared options using a decorator."""
-    func = click.option(
-        "-w", "--work-dir", default="", help="Path to working directory.", type=str
-    )(func)
-    func = click.option(
-        "-i",
-        "--instance-num",
-        default=0,
-        help="Instance number in the sequence of cluster instances (default: 0).",
-        type=int,
-    )(func)
+    for opt in reversed(
+        [
+            click.option(
+                "-w",
+                "--work-dir",
+                type=click.Path(file_okay=False, dir_okay=True, path_type=str),
+                default="",
+                show_default=True,
+                help="Path to working directory.",
+            ),
+        ]
+    ):
+        func = opt(func)
     return func
+
+
+def common_options_control(func: tp.Callable) -> tp.Callable:
+    """Add shared options to control group using a decorator."""
+    for opt in reversed(
+        [
+            click.option(
+                "-i",
+                "--instance-num",
+                type=click.IntRange(0, cli_utils.MAX_INSTANCES - 1),
+                required=True,
+                show_default=True,
+                help="Instance number in the sequence of testnet instances.",
+            ),
+        ]
+    ):
+        func = opt(func)
+    return func
+
+
+def exit_with(retval: int) -> tp.NoReturn:
+    click.get_current_context().exit(retval)
 
 
 @click.group()
@@ -32,61 +59,93 @@ def main() -> None:
 
 
 @main.command(help="Generate local testnet configuration")
-@click.option("-t", "--testnet-variant", help="Testnet variant to use.", type=str)
-@click.option("-l", "--list", is_flag=True, help="List available testnet variants and exit.")
+@click.option("-t", "--testnet-variant", type=str, help="Testnet variant to use.")
+@click.option("-l", "--ls", is_flag=True, help="List available testnet variants and exit.")
 @click.option("-r", "--run", is_flag=True, help="Run the testnet immediately (default: false).")
-@click.option("-c", "--clean", is_flag=True, help="Delete destination directory if it exists.")
+@click.option("-k", "--keep", is_flag=True, help="Don't delete destination directory if it exists.")
+@click.option(
+    "-i",
+    "--instance-num",
+    default=-1,
+    type=click.IntRange(-1, cli_utils.MAX_INSTANCES - 1),
+    show_default=True,
+    help="Instance number, auto-selected by default.",
+)
 @click.option(
     "-s",
     "--stake-pools-num",
-    type=int,
+    type=click.IntRange(3, 10),
     default=3,
-    help="Number of stake pools to create (default: 3).",
+    show_default=True,
+    help="Number of stake pools to create.",
 )
 @click.option(
-    "-p",
-    "--ports-base",
-    type=int,
-    default=23000,
-    help="Base port number (default: 23000).",
+    "-p", "--ports-base", type=int, default=23000, show_default=True, help="Base port number."
 )
+@click.option("-v", "--verbose", count=True, help="Increase verbosity (use -vv for more).")
 @common_options
+@click.pass_context
 def generate(
+    ctx: click.Context,
     testnet_variant: str,
-    list: bool,
+    ls: bool,
     run: bool,
-    clean: bool,
+    keep: bool,
+    instance_num: int,
     stake_pools_num: int,
     ports_base: int,
+    verbose: int,
     work_dir: str,
-    instance_num: int,
 ) -> None:
-    retval = cli.cmd_generate(
-        testnet_variant,
-        list,
-        run,
-        clean,
-        stake_pools_num,
-        ports_base,
-        work_dir,
-        instance_num,
+    # Check if no args were passed other than the command itself
+    if not ctx.args and not any([testnet_variant, ls]):
+        click.echo(ctx.get_help())
+        ctx.exit()
+
+    retval = cli_generate.cmd_generate(
+        testnet_variant=testnet_variant,
+        listit=ls,
+        run=run,
+        keep=keep,
+        stake_pools_num=stake_pools_num,
+        ports_base=ports_base,
+        work_dir=work_dir,
+        instance_num=instance_num,
+        verbose=verbose,
     )
-    raise SystemExit(retval)
+    ctx.exit(retval)
 
 
-@main.command(help="Control existing testnets.")
-@click.option("-l", "--list", is_flag=True, help="List running testnet instances.")
-@click.option("-s", "--stop", is_flag=True, help="Stop the running testnet.")
-@click.option("-r", "--restart", is_flag=True, help="Restart all processes of the testnet.")
-@click.option("-n", "--restart-nodes", is_flag=True, help="Restart only nodes of the testnet.")
+@main.group(help="Control existing testnets.")
+def control() -> None:
+    """Control interface for Cardonnay instances."""
+
+
+def make_control_cmd(flag_name: str, help_text: str) -> None:
+    @control.command(name=flag_name.replace("_", "-"), help=help_text)
+    @common_options_control
+    @common_options
+    def cmd(instance_num: int, work_dir: str) -> None:
+        retval = cli_control.cmd_control(
+            **{flag_name: True},
+            work_dir=work_dir,
+            instance_num=instance_num,
+        )
+        exit_with(retval)
+
+
+@control.command(name="ls", help="List running testnet instances.")
 @common_options
-def control(
-    list: bool,
-    stop: bool,
-    restart: bool,
-    restart_nodes: bool,
-    work_dir: str,
-    instance_num: int,
-) -> None:
-    retval = cli.cmd_control(list, stop, restart, restart_nodes, work_dir, instance_num)
-    raise SystemExit(retval)
+def ls(work_dir: str) -> None:
+    retval = cli_control.cmd_ls(work_dir=work_dir)
+    exit_with(retval)
+
+
+for name, help_text in [
+    ("stop", "Stop the running testnet."),
+    ("restart", "Restart all processes of the testnet."),
+    ("restart_nodes", "Restart only nodes of the testnet."),
+    ("inspect", "Inspect running testnet."),
+    ("print_env", "Print environment variables for the testnet."),
+]:
+    make_control_cmd(name, help_text)
